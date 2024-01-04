54e9238e40c05c9738d63a23ef2a04e31cf40e14
Marco Mercado <marco@stonepawtraining.com>
2024-01-04 13:55:19 -0500

    moved fetch data to a helper function

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -4,6 +4,7 @@ import Places from "./Places.jsx";
 import Error from "./Error.jsx";
 
 import { sortPlacesByDistance } from "../loc.js";
+import { fetchAvailablePlaces } from "../http.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -15,17 +16,11 @@ export default function AvailablePlaces({ onSelectPlace }) {
       setIsFetching(true);
 
       try {
-        const response = await fetch("http://localhost:3000/places");
-        const resData = await response.json();
-
-        if (!response.ok) {
-          const error = new Error("Failed to fetch places");
-          throw error;
-        }
+        const places = await fetchAvailablePlaces();
 
         navigator.geolocation.getCurrentPosition((position) => {
           const sortedPlaces = sortPlacesByDistance(
-            resData.places,
+            places,
             position.coords.latitude,
             position.coords.longitude
           );

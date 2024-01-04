1d1f8977a965ddcc623a149d1c86d7f84c8ddbd6
Marco Mercado <marco@stonepawtraining.com>
2024-01-04 13:52:08 -0500

    Sorted places based on current location

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
 import Error from "./Error.jsx";
 
+import { sortPlacesByDistance } from "../loc.js";
+
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
@@ -13,7 +15,7 @@ export default function AvailablePlaces({ onSelectPlace }) {
       setIsFetching(true);
 
       try {
-        const response = await fetch("http://localhost:3000/test");
+        const response = await fetch("http://localhost:3000/places");
         const resData = await response.json();
 
         if (!response.ok) {
@@ -21,12 +23,22 @@ export default function AvailablePlaces({ onSelectPlace }) {
           throw error;
         }
 
-        setAvailablePlaces(resData.places);
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            resData.places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
       } catch (error) {
-        setError(error);
+        setError({
+          message:
+            error.message || "Could not fetch places, please try again later",
+        });
+        setIsFetching(false);
       }
-
-      setIsFetching(false);
     }
 
     fetchPlaces();

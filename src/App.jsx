98a216c69657c8718a5beb8bc1d65a5ad50d573c
Marco Mercado <marco@stonepawtraining.com>
2024-01-09 13:15:57 -0500

    Implemented custom hook in other components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useCallback, useEffect } from "react";
+import { useRef, useState, useCallback } from "react";
 
 import Places from "./components/Places.jsx";
 import Modal from "./components/Modal.jsx";
